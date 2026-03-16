c8e72c5ba2e546ae0d0ed56e07c8fc446e0843ec
nanchiangYuan <ashleyyuan.work@gmail.com>
2026-03-15 22:17:12 -0500

    added header

--- bfs.h
+++ bfs.h
@@ -1,5 +1,17 @@
-#include <time.h>
+#ifndef BFS_H
+#define BFS_H
+
+#define FUSE_USE_VERSION 30
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <fuse.h>
 
 #define BLOCK_SIZE (512)
 #define MAX_NAME   (28)
@@ -25,7 +37,7 @@ i_bitmap_ptr        i_blocks_ptr
 */
 
 // Superblock
-struct wfs_sb {
+struct bfs_sb {
     size_t num_inodes;
     size_t num_data_blocks;
     off_t i_bitmap_ptr;         
@@ -37,7 +49,7 @@ struct wfs_sb {
 };
 
 // Inode
-struct wfs_inode {
+struct bfs_inode {
     int     num;      /* Inode number */
     mode_t  mode;     /* File type and mode */
     uid_t   uid;      /* User ID of owner */
@@ -54,11 +66,40 @@ struct wfs_inode {
 
 // Directory entry
 // 16 per block
-struct wfs_dentry {
+struct bfs_dentry {
     char name[MAX_NAME];
     int num;
 };
 
+extern int raid;
+
+extern int disk_index; // total number of disks
+extern void *disks[MAX_DISKS];
+
+// 0 if available, 1 if allocated
+extern int *raid0_dbit;
+extern int raid0_bnum;
+
+int bfs_getattr(const char *path, struct stat *stbuf);
+int bfs_mknod(const char* path, mode_t mode, dev_t rdev);
+int bfs_mkdir(const char* path, mode_t mode);
+int bfs_unlink(const char* path);
+int bfs_rmdir(const char* path);
+int bfs_read(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi);
+int bfs_write(const char* path, const char *buf, size_t size, off_t offset, struct fuse_file_info* fi);
+int bfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi);
+
+extern struct fuse_operations ops;
+
+// helper functions
+off_t find_inode(const char *path);
+off_t allocate_inode(mode_t mode);
+int find_dblock(int disk, off_t index, off_t **dblock_ptr);
+int allocate_dblock(int disk, off_t *inode, off_t **dblock_ptr);
+void indblock_init(off_t *block_loc);
+void raid0_cleardbit(off_t index);
+
+#endif
 
 /*
     notes:
